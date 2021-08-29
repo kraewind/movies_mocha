7c18b9f64f28b94795af46c9da1fe5c20676171c
krae <kraewind@gmail.com>
2021-08-29 14:47:59 -0400

    Mocha in the browser setting up chai now

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,19 @@
-it('shows an autocomplete', () => {
-
+it('shows on autocomplete', () => {
+    createAutoComplete({
+        root: document.querySelector('#target'),
+        fetchData() {
+            return [
+                {Title: 'Avengers'},
+                {Title: 'Not Avengers'},
+                {Title: 'Some other movie'}
+            ]
+        },
+        renderOption(movie) {
+            return movie.Title
+        }
+    })
+    const dropdown = document.querySelector('.dropDown')
+    it('does not show dropdown on page open', () => {
+        
+    })
 })
