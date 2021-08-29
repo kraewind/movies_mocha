3e964277699be24881a992fb99a540c1764a6b8c
krae <kraewind@gmail.com>
2021-08-29 15:18:37 -0400

    Mocha async

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,4 +1,22 @@
-it('shows on autocomplete', () => {
+const waitFor = (selector) => {
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+            if(document.querySelector(selector)){
+                clearInterval(interval)
+                clearTimeout(timeout)
+                resolve()
+            }
+        }, 30)
+        const timeout = setTimeout(() => {
+            clearInterval(interval)
+            reject()
+        }, 2000)
+    })
+}
+
+
+beforeEach(() => {
+    document.querySelector('#target').innerHTML = ''
     createAutoComplete({
         root: document.querySelector('#target'),
         fetchData() {
@@ -12,6 +30,22 @@ it('shows on autocomplete', () => {
             return movie.Title
         }
     })
+})
+
+it('dropdown starts closed', () => {
     const dropdown = document.querySelector('.dropdown')
     expect(dropdown.className).not.to.include('is-active')
+})
+
+it('After searching, dropdown opens up', () => {
+    document.querySelector('input').value = 'Avengers'
+    document.querySelector('input').dispatchEvent(new Event('input'))
+
+
+    await waitFor('.dropdown-item')
+
+    const dropdown = document.querySelector('.dropdown')
+
+    expect(dropdown.className).to.include('is-active')
+
 })
